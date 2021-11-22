4b5bd7740e1de4afb5ba5484fde4aa4cb77343b3
Juan Antonio León <jalojr@hotmail.com>
2021-11-22 21:35:01 +0000

    botón Start creado

---
+++ script.js
@@ -0,0 +1,13 @@
+var startButton = document.getElementById('game-start')
+
+startButton.addEventListener('click', clearScreen)
+
+function clearScreen () {
+    var board = document.getElementById('container')
+    var childs = document.querySelectorAll('#container > *')
+    for (let i = 0; i < childs.length; i++) {
+        board.removeChild(childs[i])
+    }
+    
+}
+
