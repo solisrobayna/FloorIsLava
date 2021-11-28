b27ba8fe7e998f520110ba61c89d71570a1a9af0
Juan Antonio León <jalojr@hotmail.com>
2021-11-28 09:42:34 +0000

    los personajes ya no se teletransportan bajo la plataforma

--- scr/main.js
+++ scr/main.js
@@ -19,18 +19,12 @@ function startGame () {
     parent.style.backgroundSize = 'contain'
     parent.style.backgroundRepeat = 'no-repeat'
     var plat1 = new Platform (500,75,150,300)
-   /* var plat2 = new Platform ('75px','150px','300px','350px','platform')
-    var plat3 = new Platform ('75px','250px','500px','250px','platform')
-    var plat4 = new Platform ('75px','350px','650px','150px','platform')*/
-    var player1 = new Player(20, 35, 350, 280, 1)
-    var player2 = new Player(20, 35, 450, 280, 2)
+    var player1 = new Player(20, 35, 350, 265, 1)
+    var player2 = new Player(20, 35, 450, 265, 2)
    
    
     var lava = new Lava (800, 100, 0, 500)
     parent.appendChild(plat1.sprite)
-    /*parent.appendChild(plat2.sprite)
-    parent.appendChild(plat3.sprite)
-    parent.appendChild(plat4.sprite)*/
     parent.appendChild(player1.sprite)
     parent.appendChild(player2.sprite)
     parent.appendChild(lava.sprite)
@@ -87,19 +81,24 @@ function startGame () {
     var timerId = setInterval(function() {  
         player1.moveX(player2)
         player1.moveY(plat1, player2)
-        player1.walkSprite()
+        if (!player1.jumping) {
+            player1.walkSprite()
+        }
         player2.moveX(player1)
         player2.moveY(plat1, player1)
-        player2.walkSprite()
+        if (!player2.jumping) {
+            player2.walkSprite()
+        }  
         player2.moveHit(player1)
         player1.moveHit(player2)
         player1.lookAt(player2)
         player2.lookAt(player1)
         lava.grow()
-        setTimeout (plat1.reduce,10000)
+        var timerPlat = setTimeout (plat1.reduce,10000)
        if (player1.collideLava(600 - lava.height) ||
             player2.collideLava(600 - lava.height)) {
             clearInterval(timerId)
+            clearTimeout(timerPlat)
             window.alert('GAME OVER')
         }
         
