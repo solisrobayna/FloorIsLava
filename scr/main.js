d77e52efb7e51c596cf053ce3de8d5d36fe3a7bf
Juan Antonio León <jalojr@hotmail.com>
2021-11-28 12:25:55 +0000

    el personaje no se puede mover al caer

--- scr/main.js
+++ scr/main.js
@@ -2,8 +2,6 @@ var startButton = document.getElementById('game-start')
 
 startButton.addEventListener('click', startGame)
 
-
-
 function clearScreen () {
     var board = document.getElementById('main')
     var childs = document.querySelectorAll('#main > *')
@@ -21,8 +19,6 @@ function startGame () {
     var plat1 = new Platform (500,75,150,300)
     var player1 = new Player(20, 35, 350, 265, 1)
     var player2 = new Player(20, 35, 450, 265, 2)
-   
-   
     var lava = new Lava (800, 100, 0, 500)
     parent.appendChild(plat1.sprite)
     parent.appendChild(player1.sprite)
@@ -79,12 +75,12 @@ function startGame () {
     })
   
     var timerId = setInterval(function() {  
-        player1.moveX(player2)
+        player1.moveX(player2, plat1)
         player1.moveY(plat1, player2)
         if (!player1.jumping) {
             player1.walkSprite()
         }
-        player2.moveX(player1)
+        player2.moveX(player1, plat1)
         player2.moveY(plat1, player1)
         if (!player2.jumping) {
             player2.walkSprite()
